49ad3c96562d60b55c562642a63d8fe8c31c8e4d
Simon O'Donnell <simonrodonnell@yahoo.co.uk>
2019-09-18 11:32:05 +0100

    encrypt guess word

--- HiddenWord.rb
+++ HiddenWord.rb
@@ -1,9 +1,19 @@
 class HiddenWord
 
-  attr_reader(:word)
+  attr_reader(:word, :display)
 
   def initialize(input_word)
     @word = input_word
+    @word_array = @word.split("")
+    @display = []
+  end
+
+  def encrypt_word()
+    length = @word_array.length()
+    while length > 0
+      @display.push("*")
+      length -= 1
+    end
   end
 
 end
--- specs/hidden_word_spec.rb
+++ specs/hidden_word_spec.rb
@@ -4,9 +4,18 @@ require_relative("../HiddenWord")
 
 class TestHiddenWord < Minitest::Test
 
-  def test_has_word()
-    hidden_word = HiddenWord.new("pineapple")
-    assert_equal("pineapple", hidden_word.word())
+  def setup
+    @hidden_word = HiddenWord.new("pineapple")
   end
+
+  def test_has_word()
+    assert_equal("pineapple", @hidden_word.word())
+  end
+
+  def test_encrypt_word
+    @hidden_word.encrypt_word()
+    assert_equal(9, @hidden_word.display.length)
+  end
+
 
 end
